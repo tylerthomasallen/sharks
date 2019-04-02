9e0c61a023180b7c9bf3475470cd33e66df4f508
Tyler <tylerthomasallen@gmail.com>
2019-04-02 15:03:26 -0700

    Update font-size

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -3,8 +3,8 @@ import source from '../../assets/video/sharks_2017.mp4';
 
 const Video = () => {
   return(
-    <div className="video-container">
-      <video id="video"autoPlay loop muted muted controls>
+    <div className="video-container" id="video">
+      <video autoPlay loop muted muted controls>
         <source src={source} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
